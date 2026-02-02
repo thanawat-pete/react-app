2ecbddcbedaa14648e193d71abd41f571ffe7851
Thanawat-Pete <petelov3dinosaurth@gmail.com>
2026-02-02 10:35:28 +0700

    Add Prettier for code formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-app/',
+  base: "/react-app/",
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  }
-})
+  },
+});
